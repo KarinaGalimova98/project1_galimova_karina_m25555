ba5537a2f9f2eed9108b142753f3712a8dc4cc86
Karina Galimova <karina@example.com>
2025-11-28 12:16:27 +0000

    1.4

--- labyrinth_game/main.py
+++ labyrinth_game/main.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
 
+"""Entry point for the labyrinth game."""
+
+
 def main() -> None:
+    """Run the labyrinth game."""
     print("Первая попытка запустить проект!")
 
 
 if __name__ == "__main__":
     main()
+
